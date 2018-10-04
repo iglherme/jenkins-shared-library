04d24dbfbdcd939689886fc57650a258f114477e
iglherme <iglherme@gmail.com>
2018-10-04 14:05:17 -0300

    Update slackNotification.groovy

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -2,24 +2,24 @@
 
 def call(String buildResult, String channel) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", channel: channel, message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful. \nCheck results <${env.RUN_DISPLAY_URL}|here>."
+    slackSend color: "good", channel: channel, message: "*Job:* `${env.JOB_NAME}` with buildnumber *${env.BUILD_NUMBER}* was successful. \nCheck results <${env.RUN_DISPLAY_URL}|here>."
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger",channel: channel, message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed. \nCheck results <${env.RUN_DISPLAY_URL}|here>. "
+    slackSend color: "danger",channel: channel, message: "*Job:* `${env.JOB_NAME}` with buildnumber *${env.BUILD_NUMBER}* was failed. \nCheck results <${env.RUN_DISPLAY_URL}|here>. "
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning",channel: channel, message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable. \nCheck results <${env.RUN_DISPLAY_URL}|here>."
+    slackSend color: "warning",channel: channel, message: "*Job:* `${env.JOB_NAME}` with buildnumber *${env.BUILD_NUMBER}* was unstable. \nCheck results <${env.RUN_DISPLAY_URL}|here>."
   }
   else if( buildResult == "APPROVE" ) { 
-    slackSend color: "#0031bb",channel: channel, message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} requires approval. \nPerform this action <${env.RUN_DISPLAY_URL}|here>."
+    slackSend color: "#0031bb",channel: channel, message: "*Job:* `${env.JOB_NAME}` with buildnumber *${env.BUILD_NUMBER}* requires approval. \nPerform this action <${env.RUN_DISPLAY_URL}|here>."
   }
   else if( buildResult == "ATTENTION" ) { 
-    slackSend color: "#0031bb",channel: channel, message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} requires manual action. \nPerform this action <${env.RUN_DISPLAY_URL}|here>."
+    slackSend color: "#0031bb",channel: channel, message: "*Job:* `${env.JOB_NAME}` with buildnumber *${env.BUILD_NUMBER}* requires manual action. \nPerform this action <${env.RUN_DISPLAY_URL}|here>."
   }
   else if( buildResult == "START" ) { 
-    slackSend color: '#909090',channel: channel, message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} started. \nCheck status <${env.RUN_DISPLAY_URL}|here>."
+    slackSend color: '#909090',channel: channel, message: "*Job:* `${env.JOB_NAME}` with buildnumber *${env.BUILD_NUMBER}* started. \nCheck status <${env.RUN_DISPLAY_URL}|here>."
   }   
   else {
-    slackSend color: "danger",channel: channel, message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear. \nCheck results <${env.RUN_DISPLAY_URL}|here>."	
+    slackSend color: "danger",channel: channel, message: "*Job:* `${env.JOB_NAME}` with buildnumber *${env.BUILD_NUMBER}* its resulat was unclear. \nCheck results <${env.RUN_DISPLAY_URL}|here>."	
   }
 }
